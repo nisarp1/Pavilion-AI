d89c80c43862c17e6b93b14fffbbba2b178fa62d
Nisar P <nisar@Nisars-MacBook-Air.local>
2025-12-02 15:05:00 +0530

    Deploy fixed version to Railway

--- backend/pavilion_gemini/settings.py
+++ backend/pavilion_gemini/settings.py
@@ -177,6 +177,8 @@ if not CORS_ALLOWED_ORIGINS and DEBUG:
         'http://127.0.0.1:3000',
         'http://localhost:3001',
         'http://127.0.0.1:3001',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
     ]
 
 CORS_ALLOW_CREDENTIALS = True
