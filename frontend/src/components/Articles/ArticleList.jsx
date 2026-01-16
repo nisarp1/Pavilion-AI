4d3321c413e259fa82cd9ce74a3ad01d221d638b
Nisar P <nisar@Nisars-MacBook-Air.local>
2026-01-16 10:41:11 +0530

    Update ArticleList View link to pavilionend.in

--- frontend/src/components/Articles/ArticleList.jsx
+++ frontend/src/components/Articles/ArticleList.jsx
@@ -655,7 +655,7 @@ function ArticleList() {
                               <>
                                 <span>|</span>
                                 <a
-                                  href={`https://phpstack-1448755-6044488.cloudwaysapps.com/${article.slug}/`}
+                                  href={`https://pavilionend.in/${article.slug}/`}
                                   target="_blank"
                                   rel="noopener noreferrer"
                                   className="hover:text-primary-600"
